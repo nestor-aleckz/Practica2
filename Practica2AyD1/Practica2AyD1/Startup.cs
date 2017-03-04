4a30074d3bb5d72d2e2ca69f85ccd891a5e0918e
ranjua <randy_juarez@hotmail.com>
2017-03-03 19:29:03 -0600

    Inicio de aplicacion

---
+++ Practica2AyD1/Practica2AyD1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practica2AyD1.Startup))]
+namespace Practica2AyD1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
